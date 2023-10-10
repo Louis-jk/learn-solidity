097d4ecf2a2a88e938e7060cf48234943440e1e9
Juno Kim <lippoint.surf0622@gmail.com>
2023-10-10 23:22:40 +0900

    mapping

---
+++ lecture/lec17.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Lec17 {
+
+    mapping(uint256 => uint256) private ageList;
+
+    function setAgeList(uint256 _index, uint256 _age) public {
+        ageList[_index] = _age;
+    }
+
+    function getAge(uint256 _index) public view returns(uint256) {
+        return ageList[_index];
+    }
+
+}
